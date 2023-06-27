87897e2c4e521bebe51a344c429e51cbe9a2e3d6
Penzhi <tyypz2590477658@gmail.com>
2023-06-27 22:01:10 +0200

    pylint

--- tests/test_training_model.py
+++ tests/test_training_model.py
@@ -1,7 +1,7 @@
 """
 This module contains tests for training the model of the project.
 """
-import pytest 
+import pytest
 import joblib
 from sklearn.naive_bayes import GaussianNB
 from src.preprocessing import pre_process
@@ -12,12 +12,12 @@ def trained_model():
     """
     This fixture loads a trained model for testing.
     """
-    trained_model = joblib.load('data/models/c2_Classifier_Sentiment_Model')
-    yield trained_model
+    model = joblib.load('data/models/c2_Classifier_Sentiment_Model')
+    yield model
 
 def test_model_robustness(trained_model):
     """
-    Test the robustness of the model by comparing 
+    Test the robustness of the model by comparing
     the performances with different seeds.
     """
     _, X_test, _, y_test = pre_process(10)
@@ -25,7 +25,6 @@ def test_model_robustness(trained_model):
     for seed in [1, 2]:
         X_train, X_test, y_train, y_test = pre_process(seed)
         classifier = GaussianNB()
-        Classifier = classifier.fit(X_train, y_train)
+        trained_classifier = classifier.fit(X_train, y_train)
         acc, _ = evaluate_model()
         assert abs(acc_origin - acc) <= 0.1
-
