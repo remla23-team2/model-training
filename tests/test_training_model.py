34be5adba51dc100cafa6b032d1827287604a24a
Ivor <i.zagorac@student.tudelft.nl>
2023-06-27 20:38:14 +0200

    Implement robustness test

--- tests/test_training_model.py
+++ tests/test_training_model.py
@@ -6,18 +6,13 @@ from src.preprocessing import pre_process
 from src.evaluate import evaluate_model
 from sklearn.naive_bayes import GaussianNB
 
-@pytest.fixture()
-def trained_model():
-    trained_model = joblib.load('data/models/c2_Classifier_Sentiment_Model')
-    yield trained_model
-
-def test_model_robustness(trained_model):
-    _, X_test, _, y_test = pre_process(10)
+def test_model_robustness():
+    _, X_test, _, y_test = pre_process()
     acc_origin, _ = evaluate_model()
     for seed in [1, 2]:
-        X_train, X_test, y_train, y_test = pre_process(seed)
+        X_train, X_test, y_train, y_test = pre_process(seed=seed)
         classifier = GaussianNB()
-        Classifier = classifier.fit(X_train, y_train)
-        acc, _ = evaluate_model()
-        assert abs(acc_origin - acc) <= 0.1
+        classifier = classifier.fit(X_train, y_train)
+        acc, _ = evaluate_model(X_test=X_test, y_test=y_test)
+        assert abs(acc_origin - acc) <= 0.15
 
